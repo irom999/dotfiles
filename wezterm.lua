d683259f02819f4ea75ecfb9b6e49cfc28758977
irom999 <morizei0719@gmail.com>
2026-03-09 17:42:15 +0900

    Initial commit: WezTerm config

---
+++ wezterm.lua
@@ -0,0 +1,105 @@
+local wezterm = require("wezterm")
+local config = wezterm.config_builder()
+
+config.default_prog = { "pwsh.exe" }
+
+config.automatically_reload_config = true
+config.font_size = 12.0
+config.use_ime = true
+config.window_background_opacity = 0.85
+config.macos_window_background_blur = 20
+
+config.use_fancy_tab_bar = true
+----------------------------------------------------
+-- Tab
+----------------------------------------------------
+-- タイトルバーを非表示
+config.window_decorations = "RESIZE"
+-- タブバーの表示
+config.show_tabs_in_tab_bar = true
+-- タブが一つの時は非表示
+config.hide_tab_bar_if_only_one_tab = false
+-- falseにするとタブバーの透過が効かなくなる
+-- config.use_fancy_tab_bar = false
+
+-- タブバーの透過
+config.window_frame = {
+  inactive_titlebar_bg = "none",
+  active_titlebar_bg = "none",
+}
+
+-- タブバーを背景色に合わせる
+config.window_background_gradient = {
+  colors = { "#000000" },
+}
+
+config.front_end = "WebGpu"
+config.window_decorations = 'INTEGRATED_BUTTONS'
+config.window_background_opacity = 0.85
+config.text_background_opacity = 0.9
+config.animation_fps = 60
+config.win32_system_backdrop = "Auto"
+config.default_cursor_style = "BlinkingBar"
+config.cursor_blink_rate = 500
+config.cursor_blink_ease_in = 'Constant'
+config.cursor_blink_ease_out = 'Constant'
+config.window_frame = {
+    inactive_titlebar_bg = 'None',
+    active_titlebar_bg = 'None',
+}
+config.colors = {
+    tab_bar = {
+        inactive_tab_edge = "None"
+    }
+}
+config.inactive_pane_hsb = {
+    saturation = 0.9,
+    brightness = 0.8,
+}
+
+-- タブ同士の境界線を非表示
+config.colors = {
+  tab_bar = {
+    inactive_tab_edge = "none",
+  },
+}
+
+-- タブの形をカスタマイズ
+-- タブの左側の装飾
+local SOLID_LEFT_ARROW = wezterm.nerdfonts.ple_lower_right_triangle
+-- タブの右側の装飾
+local SOLID_RIGHT_ARROW = wezterm.nerdfonts.ple_upper_left_triangle
+
+wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
+  local background = "#5c6d74"
+  local foreground = "#FFFFFF"
+  local edge_background = "none"
+  if tab.is_active then
+    background = "#ae8b2d"
+    foreground = "#FFFFFF"
+  end
+  local edge_foreground = background
+  local title = "   " .. wezterm.truncate_right(tab.active_pane.title, max_width - 1) .. "   "
+  return {
+    { Background = { Color = edge_background } },
+    { Foreground = { Color = edge_foreground } },
+    { Text = SOLID_LEFT_ARROW },
+    { Background = { Color = background } },
+    { Foreground = { Color = foreground } },
+    { Text = title },
+    { Background = { Color = edge_background } },
+    { Foreground = { Color = edge_foreground } },
+    { Text = SOLID_RIGHT_ARROW },
+  }
+end)
+
+----------------------------------------------------
+-- keybinds
+----------------------------------------------------
+config.disable_default_key_bindings = true
+config.keys = require("keybinds").keys
+config.key_tables = require("keybinds").key_tables
+config.leader = { key = "q", mods = "CTRL", timeout_milliseconds = 2000 }
+
+return config
+
